9a7bac7e40d0228b1b987bffafdcdfbb552851cb
Konstantinosmrts <57006990+Konstantinosmrts@users.noreply.github.com>
2022-02-18 21:43:26 +0200

    bug fixes

--- Javascript/General.js
+++ Javascript/General.js
@@ -34,6 +34,7 @@ function filterFunction() {
     }
 }
 
+
 function PopUp(hideOrshow) {
     if (hideOrshow == 'hide') document.getElementById('ac-wrapper').style.display = "none";
     else document.getElementById('ac-wrapper').removeAttribute('style');
