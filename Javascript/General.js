3154f9c2aa802eaa1fe48d613abac97337b49614
Konstantinosmrts <57006990+Konstantinosmrts@users.noreply.github.com>
2022-02-24 14:05:12 +0200

    bug fixes

--- Javascript/General.js
+++ Javascript/General.js
@@ -38,3 +38,5 @@ var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-tog
 var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
     return new bootstrap.Dropdown(dropdownToggleEl)
 })
+
+
